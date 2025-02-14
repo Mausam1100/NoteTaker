8f0b8256bb9283473ee0c2589497a4700cb14f53
Mausam Baduwal <mausambaduwal303@gmail.com>
2025-02-14 22:36:59 +0545

    t

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,13 @@
 const express = require("express")
 const app = express()
-// const cors = require("cors")
+const cors = require("cors")
 const cookieParser = require('cookie-parser')
 
-// app.use(cors({
-//     origin: 'https://notetaker-client.vercel.app',
-//     credentials: true,  // Allow credentials (cookies, etc.)
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// }));
-
-const cors = require('cors');
-const corsOptions ={
-    origin:'https://notetaker-client.vercel.app', 
-    credentials:true,
+app.use(cors({
+    origin: 'https://notetaker-client.vercel.app',
+    credentials: true,  // Allow credentials (cookies, etc.)
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}
-app.use(cors(corsOptions));
+}));
 
 app.use(express.json())
 app.use(cookieParser())
