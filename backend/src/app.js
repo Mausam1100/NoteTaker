02656bbb53cabfc92c5bfa0642ad856b9396c86f
Mausam Baduwal <mausambaduwal303@gmail.com>
2025-02-14 22:05:49 +0545

    7

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,10 @@ const cookieParser = require('cookie-parser')
 
 app.use(cors({
     origin: 'https://notetaker-client.vercel.app',  // Allow only this specific origin
-    credentials: true  // Allow credentials (cookies, etc.)
-}));
+    credentials: true,  // Allow credentials (cookies, etc.)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow the necessary HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],  // Allow the necessary headers
+  }));
 
 app.use(express.json())
 app.use(cookieParser())
