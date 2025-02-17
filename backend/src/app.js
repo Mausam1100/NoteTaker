239cf0094e7c0c10db72084da0b45ed360987d52
Mausam Baduwal <mausambaduwal303@gmail.com>
2025-02-17 19:38:06 +0545

    add ors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const cookieParser = require('cookie-parser')
 
 app.use(cors({
-    origin: 'http://localhost:5175',
+    origin: 'https://notetaker-client.onrender.com',
     credentials: true, 
 }));
 
