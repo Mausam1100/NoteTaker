194cbbaf555019af1946ef4ddb34a79c9dc2b97a
Mausam Baduwal <mausambaduwal303@gmail.com>
2025-02-17 20:21:40 +0545

    vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const cookieParser = require('cookie-parser')
 
 app.use(cors({
-    origin: 'https://notetaker-client.onrender.com',
+    origin: 'https://notetaker-client.vercel.app',
     credentials: true, 
 }));
 
